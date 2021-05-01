58a892d4396dd7e68d3bcc20ec412a0b692ccfb3
duongbui123 <buiducduong2342000@gmail.com>
2021-05-01 16:02:21 +0700

    remove redux-logger from application build

--- crown_clothing/src/components/redux/store.js
+++ crown_clothing/src/components/redux/store.js
@@ -4,9 +4,13 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const applymiddleware = [logger];
+const middleWare = [];
 
-const store = createStore(rootReducer, applyMiddleware(...applymiddleware));
+if (process.env.NODE_ENV === 'development') {
+  middleWare.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleWare));
 
 const persistor = persistStore(store);
 
